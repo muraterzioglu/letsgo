ee431de03de453292ecff4f6c6f690b13eb15ac3
Murat Terzioğlu <its.muraterzioglu@gmail.com>
2022-06-08 19:53:23 +0300

    Error Handling

--- first_module/home.go
+++ first_module/home.go
@@ -1,6 +1,9 @@
 package first_module
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
@@ -9,9 +12,14 @@ func Hello(name string) string {
 	return message
 }
 
-func HelloAge(name string, age string) string {
+func HelloAge(name string, age string) (string, error) {
+	if name == "" {
+		return "", errors.New("name cannot be empty")
+	}
+
 	message := fmt.Sprintf("Hi, %v. You are %s years old today!", name, age)
-	return message
+	// nil means no error in go
+	return message, nil
 }
 
 // In Go, the := operator is a shortcut for declaring and initializing a variable in one line
