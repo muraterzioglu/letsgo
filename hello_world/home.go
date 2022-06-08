c91c33126279094a91505a94ab05f99bb171c54e
Murat Terzioğlu <its.muraterzioglu@gmail.com>
2022-06-08 19:15:42 +0300

    Package & Module System + Calling my modules

--- hello_world/home.go
+++ hello_world/home.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
-
-/* Or:
 import (
 	"fmt"
+	firstModule "letsgo/first_module"
 	"rsc.io/quote"
 )
+
+/* Or:
+import "fmt"
+import "rsc.io/quote"
 */
 
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
+
+	msg := firstModule.Hello("Murat")
+	fmt.Println(msg)
+
+	msgAge := firstModule.HelloAge("Murat", "18")
+	fmt.Println(msgAge)
 }
