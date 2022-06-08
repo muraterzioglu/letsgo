dab478d876fdc47c31d18e20a3f43a6558d66c61
Murat Terzioğlu <its.muraterzioglu@gmail.com>
2022-06-08 20:59:07 +0300

    Adding error check for empty name

--- greetings/home.go
+++ greetings/home.go
@@ -6,11 +6,15 @@ import (
 )
 
 // JustName returns a greeting for the named person.
-func JustName(name string) string {
+func JustName(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name cannot be empty")
+	}
+
 	// Return a greeting that embeds the name in a message.
 	// In Go, the := operator is a shortcut for declaring and initializing a variable in one line
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
 
 /*
