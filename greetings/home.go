12ef0dacf345b54c1f945ed951a1d241f9b7e9ac
Murat Terzioğlu <its.muraterzioglu@gmail.com>
2022-06-08 20:38:18 +0300

    Moving Random Greeting function to another file

--- greetings/home.go
+++ greetings/home.go
@@ -3,8 +3,6 @@ package greetings
 import (
 	"errors"
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 // JustName returns a greeting for the named person.
@@ -27,33 +25,4 @@ func WithAge(name string, age string) (string, error) {
 	message := fmt.Sprintf("Hi, %v. You are %s years old today!", name, age)
 	// nil means no error in go
 	return message, nil
-}
-
-func WithRandom(name string) (string, error) {
-	if name == "" {
-		return "", errors.New("name cannot be empty")
-	}
-
-	message := fmt.Sprintf(randomFormat(), name)
-	return message, nil
-}
-
-// init sets initial values for variables used in the function.
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-// randomFormat returns one of a set of greeting messages. The returned
-// message is selected at random.
-func randomFormat() string {
-	// A slice of message formats.
-	formats := []string{
-		"Hi, %v. Welcome!",
-		"Great to see you, %v!",
-		"Hail, %v! Well met!",
-	}
-
-	// Return a randomly selected message format by specifying
-	// a random index for the slice of formats.
-	return formats[rand.Intn(len(formats))]
 }
